94468b6c05046a0bf72e35169139eb80abcbc6ec
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-02-23 15:45:19 +0900

    動かなかったので再度

--- qamp.js
+++ qamp.js
@@ -149,10 +149,10 @@ define(['pipAPI', 'underscore'], function (APIConstructor, _) {
 
 			//Instructions text for the 2-responses version.
 			exampleBlockInst: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' +
-				'まず，文章が呈示され，次に図形が呈示されるので文章は無視して図形がどちらかというと視覚的にrightAttribute場合は，<B>rightKey</B>を押してください。' +
-				'また，呈示される図形が視覚的にleftAttribute場合は<b>leftKey</b> を押してください。<br/><br/>' +
+				'まず，文章が呈示され，次に図形が呈示されるので文章は無視して図形がどちらかというと視覚的に rightAttribute 場合は，<B>rightKey</B>を押してください。' +
+				'また，呈示される図形が視覚的に leftAttribute 場合は<b>leftKey</b> を押してください。<br/><br/>' +
 				'文章は素早く現れて，すぐに消えます。  ' +
-				'targetCatの前に呈示される文章は無視し， targetCatのみを評価することを忘れないでください.<br/><br/></p>' +
+				'targetCat の前に呈示される文章は無視し， targetCat のみを評価することを忘れないでください.<br/><br/></p>' +
 				'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' +
 				'When you are ready to try a few practice responses, hit the <b>space bar</b>.</p>' +
 				'<p style="font-size:12px; text-align:center; font-family:arial">' +
