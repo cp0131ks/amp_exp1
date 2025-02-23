298d8b568d2548b0c4fed67b51a3583f138fd41f
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-02-23 17:06:38 +0900

    imageNumChange
    amp.jsのimageをBlackとWhiteでそれぞれ2つに削減

--- amp.js
+++ amp.js
@@ -11,17 +11,8 @@ define(['pipAPI', 'https://cp0131ks.github.io/amp_exp1/qamp.js'], function(APICo
 				//An array of all media objects for this category.
 				mediaArray : [
 				    {image : 'b01.JPG'}, 
-					{image : 'b02.JPG'}, 
-					{image : 'b03.JPG'}, 
-					{image : 'b04.JPG'}, 
-					{image : 'b05.JPG'}, 
-					{image : 'b06.JPG'}, 
-					{image : 'b07.JPG'}, 
-					{image : 'b08.JPG'}, 
-					{image : 'b09.JPG'}, 
-					{image : 'b10.JPG'}, 
-					{image : 'b11.JPG'}, 
-					{image : 'b12.JPG'}]
+					{image : 'b02.JPG'}
+				]
 
 			}, 
 			{
@@ -30,17 +21,8 @@ define(['pipAPI', 'https://cp0131ks.github.io/amp_exp1/qamp.js'], function(APICo
 				//An array of all media objects for this category.
 				mediaArray : [
 				    {image : 'w01.JPG'}, 
-					{image : 'w02.JPG'}, 
-					{image : 'w03.JPG'}, 
-					{image : 'w04.JPG'}, 
-					{image : 'w05.JPG'}, 
-					{image : 'w06.JPG'}, 
-					{image : 'w07.JPG'}, 
-					{image : 'w08.JPG'}, 
-					{image : 'w09.JPG'}, 
-					{image : 'w10.JPG'}, 
-					{image : 'w11.JPG'}, 
-					{image : 'w12.JPG'}]
+					{image : 'w02.JPG'}
+				]
 			}
 		],
 
