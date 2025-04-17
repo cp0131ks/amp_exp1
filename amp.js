2a041a532db280775d27ecc086bc98ded69e32bb
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-04-17 19:28:30 +0900

    文章群を削除
    AMP_exp1は画像群のみにするので文章群を削除した

--- amp.js
+++ amp.js
@@ -29,40 +29,7 @@ define(['pipAPI', 'https://cp0131ks.github.io/amp_exp1/qamp.js'], function(APICo
 					{image : 'wo04.jpg'}
 				]
 			},
-			{
-				nameForFeedback : 'Manwd',  //Will be used in the user feedback 
-				nameForLogging : 'Manwd', //Will be used in the logging
-				//An array of all media objects for this category.
-				mediaArray : [
-				    {word: '男性は傲慢だ'},
-                    {word: '男性は子どもらしい'},
-					{word: '男性は成熟している'},
-                    {word: '男性は感情的だ'},
-					{word: '男性は傲慢であるべきだ'},
-                    {word: '男性は子どもらしくあるべきだ'},
-					{word: '男性は成熟しているべきだ'},
-                    {word: '男性は感情的であるべきだ'}
-                  
-				]
-
-			}, 
-			{
-				nameForFeedback : 'Womanwd',  //Will be used in the user feedback 
-				nameForLogging : 'Womanwd', //Will be used in the logging
-				//An array of all media objects for this category.
-				mediaArray : [
-				    {word: '女性は傲慢だ'},
-                    {word: '女性は子どもらしい'},
-					{word: '女性は成熟している'},
-                    {word: '女性は感情的だ'},
-					{word: '女性は傲慢であるべきだ'},
-                    {word: '女性は子どもらしくあるべきだ'},
-					{word: '女性は成熟しているべきだ'},
-                    {word: '女性は感情的であるべきだ'}
-                  
-				]
-
-			}, 
+			
 
 		
 		],
