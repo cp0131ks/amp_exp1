d010550298cfed4f35b325cce82e26f341d7cec9
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-04-20 01:03:41 +0900

    刺激の追加

--- amp.js
+++ amp.js
@@ -13,7 +13,11 @@ define(['pipAPI', 'https://cp0131ks.github.io/amp_exp1/qamp.js'], function(APICo
 				    {image : 'm01.jpg'}, 
 					{image : 'm02.jpg'},
 					{image : 'm03.jpg'},
-					{image : 'm04.jpg'}					
+					{image : 'm04.jpg'},
+					{image : 'm05.jpg'}, 
+					{image : 'm06.jpg'},
+					{image : 'm07.jpg'},
+					{image : 'm08.jpg'}					
                   
 				]
 
@@ -26,7 +30,11 @@ define(['pipAPI', 'https://cp0131ks.github.io/amp_exp1/qamp.js'], function(APICo
 				    {image : 'wo01.jpg'}, 
 					{image : 'wo02.jpg'},
 					{image : 'wo03.jpg'},
-					{image : 'wo04.jpg'}
+					{image : 'wo04.jpg'},
+					{image : 'wo05.jpg'}, 
+					{image : 'wo06.jpg'},
+					{image : 'wo07.jpg'},
+					{image : 'wo08.jpg'}
 				]
 			},
 			
